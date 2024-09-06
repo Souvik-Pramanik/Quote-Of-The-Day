073c97c248a0e880eb38e3a7f7a32ee642e83160
Souvik Pramanik <54247555+Souvik-Pramanik@users.noreply.github.com>
2024-09-07 00:56:21 +0530

    update

--- js/app.js
+++ js/app.js
@@ -5,7 +5,33 @@ document.addEventListener("DOMContentLoaded", () => {
   const button = document.getElementById("button");
   const searchButton = document.getElementById("searchButton");
   const authorInput = document.getElementById("authorInput");
+  const installButton = document.getElementById("install-button");
+  let deferredPrompt;
 
+  window.addEventListener("beforeinstallprompt", (e) => {
+    // Prevent the mini-infobar from appearing on mobile
+    e.preventDefault();
+    // Stash the event so it can be triggered later.
+    deferredPrompt = e;
+    // Show the install button
+    installButton.style.display = 'block';
+
+    installButton.addEventListener('click', () => {
+      // Hide the install button
+      installButton.style.display = 'none';
+      // Show the install prompt
+      deferredPrompt.prompt();
+      deferredPrompt.userChoice.then((choiceResult) => {
+        if (choiceResult.outcome === 'accepted') {
+          console.log('User accepted the A2HS prompt');
+        } else {
+          console.log('User dismissed the A2HS prompt');
+        }
+        deferredPrompt = null;
+      });
+    });
+  });
+  
   const colors = [
     "#eeb76b",
     "#FFC720",
